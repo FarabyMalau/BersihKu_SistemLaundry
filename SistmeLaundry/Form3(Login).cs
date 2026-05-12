737558a4a10146833785ccebf31b9c8e85cb9f03
FarabyMalau <farabymuzakki49@gmail.com>
2026-05-12 20:10:30 +0700

    inisialisasi koneksi database untuk autentikasi

--- SistmeLaundry/Form3(Login).cs
+++ SistmeLaundry/Form3(Login).cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistmeLaundry
+{
+    public partial class Form3_Login_ : Form
+    {
+        private SqlConnection conn =
+       new SqlConnection("Data Source=DZAKNERZ\\DATABASEABY;Initial Catalog=DBBersihKu;Integrated Security=True");
+
+        public Form3_Login_()
+        {
+            InitializeComponent();
+        }
