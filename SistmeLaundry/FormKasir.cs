338673ebb0d1a8dbed8572e43b2944f3141b904d
FarabyMalau <farabymuzakki49@gmail.com>
2026-05-12 20:21:17 +0700

    Menambahkan validasi input transaksi

--- SistmeLaundry/FormKasir.cs
+++ SistmeLaundry/FormKasir.cs
@@ -55,4 +55,49 @@ void HitungTotalHarga()
         {
             HitungTotalHarga();
         }
+
+
+        // ================= VALIDASI KOSONG =================
+if (txtP.Text == "" || txtk.Text == "" || txtkp.Text == "" || txtb.Text == "" || txtth.Text == "")
+{
+    MessageBox.Show("Data belum lengkap");
+    return;
+}
+
+// ================= VALIDASI NAMA =================
+if (!System.Text.RegularExpressions.Regex.IsMatch(txtP.Text, @"^[a-zA-Z\s]+$"))
+{
+    MessageBox.Show("Nama pelanggan tidak boleh mengandung simbol!");
+    return;
+}
+
+if (!System.Text.RegularExpressions.Regex.IsMatch(txtk.Text, @"^[a-zA-Z\s]+$"))
+{
+    MessageBox.Show("Nama kasir tidak boleh mengandung simbol!");
+    return;
+}
+
+// ================= VALIDASI ANGKA =================
+decimal harga, berat;
+
+if (!decimal.TryParse(txtth.Text, out harga) ||
+    !decimal.TryParse(txtb.Text, out berat))
+{
+    MessageBox.Show("Harga dan berat harus berupa angka!");
+    return;
+}
+
+// ================= VALIDASI TIDAK BOLEH MINUS =================
+if (harga <= 0 || berat <= 0)
+{
+    MessageBox.Show("Harga dan berat tidak boleh nol atau minus!");
+    return;
+}
+
+// ================= VALIDASI TANGGAL =================
+if (dtpT.Value.Year > DateTime.Now.Year)
+{
+    MessageBox.Show("Tahun tidak boleh lebih dari sekarang!");
+    return;
+}
     }
