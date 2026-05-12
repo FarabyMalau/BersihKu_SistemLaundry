6935f1b617f203e61f823133a8e92c538191fa56
FarabyMalau <farabymuzakki49@gmail.com>
2026-05-12 20:20:01 +0700

    Membuat form laundry dan koneksi database

--- SistmeLaundry/FormKasir.cs
+++ SistmeLaundry/FormKasir.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistmeLaundry
+{
+    public partial class p1 : Form
+    {
+        private SqlConnection conn =
+        new SqlConnection("Data Source=DZAKNERZ\\DATABASEABY;Initial Catalog=DBBersihKu;Integrated Security=True");
+
+        public p1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtth.ReadOnly = true;
+        }
+    }
+}
